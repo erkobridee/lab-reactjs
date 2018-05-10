25d643f8b445da9a55bdfcd9368f2baf133593a7
Erko Bridee <erko.bridee@gmail.com>
2018-05-10 00:48:59 -0300

    add the initial redux code support

--- projects/first-contact_redux-counter/src/App.js
+++ projects/first-contact_redux-counter/src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 
 import Counter from './Counter';
 
 import logo from './logo.svg';
 import './App.css';
 
+//----------------------------------------------------------------------------//
+// @begin: redux logic
+
+import { createStore } from 'redux';
+
+function reducer(){
+  return {
+    count : 42
+  };
+}
+
+const store = createStore(reducer);
+
+// @end: redux logic
+//----------------------------------------------------------------------------//
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">First React + Redux concat : a dumb test</h1>
-        </header>
-        <Counter count={42} />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">First React + Redux concat : a dumb test</h1>
+          </header>
+          <Counter count={42} />
+        </div>
+      </Provider>
     );
   }
 }
